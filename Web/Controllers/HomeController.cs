51ef5d4d0c1f852527dd7382753ba418599591d8
invisy <invisy@users.noreply.github.com>
2021-04-26 01:35:19 +0300

    Changed namespaces, added some models & viewmodels

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using Airlines.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Web.Models;
 
-namespace Web.Controllers
+namespace Airlines.Web.Controllers
 {
     public class HomeController : Controller
     {
---
+++ Web/Models/FindFlightViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Airlines.Web.Models
+{
+    public class FindFlightViewModel
+    {
+        public List<SelectListItem> DepartureCities { get; set; }
+        public List<SelectListItem> IncomingCities { get; set; }
+        public int? SelectedDepartureCityId { get; set; }
+        public int? SelectedIncomingCityId { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public DateTime? IncomingDate { get; set; }
+    }
+}
